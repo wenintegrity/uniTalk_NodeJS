4df79688553e02dc4c4b7b7a827902fe559afe68
daniil.mishchenko <eUT3lsPYWvFZ>
2018-02-26 14:47:47 +0200

    Add full validation request in POST/calculations

--- api/router/data.js
+++ api/router/data.js
@@ -3,41 +3,29 @@ const Calculation                       = require('../calculation');
 const queries                           = require('../model/queries');
 const db                                = require('../model/db');
 const {check, validationResult}         = require('express-validator/check');
-
-const arrValidation = [
-    check('data.data_1')
-        .not().isEmpty()
-        .exists(),
-    check('data.data_2')
-        .not().isEmpty()
-        .exists(),
-    check('data.data_3')
-        .not().isEmpty()
-        .exists()
-];
+const validPostCalc                     = require('./validations').validPostCalc(check);
 
 
-router.post('/calculations', arrValidation, (req, res, next) => {
-
+router.post('/calculations', validPostCalc, (req, res, next) => {
     if (!validationResult(req).isEmpty()) {
         let _error = new Error('Input data have error');
         _error.status = 400;
         next(_error);
-    }
-
-    Calculation(req.body.data)
-        .then((data) => {
-            return queries.insertDoc(db.getConnect(), {
-                phone_id: req.body.id,
-                reqBody: req.body,
-                calcData: data
+    } else {
+        Calculation(req.body.data)
+            .then((data) => {
+                return queries.insertDoc(db.getConnect(), {
+                    phone_id: req.body.id,
+                    reqBody: req.body,
+                    calcData: data
+                })
+                    .then((saveData) => {
+                        res.set('Access-Control-Allow-Origin', '*');
+                        return res.json(saveData.calcData.result.result_1);
+                    });
             })
-                .then((saveData) => {
-                    res.set('Access-Control-Allow-Origin', '*');
-                    return res.json(saveData.calcData.result.result_1);
-                });
-        })
-        .catch(next);
+            .catch(next);
+    }
 });
 
 router.get('/calculations/last', (req, res, next) => {
