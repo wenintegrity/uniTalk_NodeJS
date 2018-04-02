cacdf5d982159ab5cfca82548288b1632004ad44
daniil <daniil.mishchenko@chisw.com>
2018-04-02 17:21:35 +0300

    Change routers...

--- routers/calculation.route.js
+++ routers/calculation.route.js
@@ -58,6 +58,13 @@ router.post('/calculations/:calc_id/picture', (req, res, next) => {
     .catch(next)
 })
 
+router.get('/calculations/last', (req, res, next) => {
+  Calculation.findOne().sort({_id: 1}).lean()
+    .then(document => {
+      return res.json(document)
+    }).catch(next)
+})
+
 router.get('/calculations/:id', (req, res, next) => {
   Calculation.findById(req.params.id).lean()
     .then((document) => {
@@ -84,8 +91,11 @@ router.get('/users/:user_id/sessions', (req, res, next) => {
 
 router.get('/session/:session_id', (req, res, next) => {
   Session.findById(req.params.session_id).lean()
-    .then((document) => {
-      return res.json(document)
+    .then((session) => {
+      return Calculation.find({'_id': {'$in': session.calculations}}).select('_id, user_id pictures req.location req.time').lean()
+    })
+    .then((calculations) => {
+      res.status(200).send(calculations)
     })
     .catch(next)
 })
@@ -111,41 +121,5 @@ router.get('/calculations/:id/data/:data_id', (req, res, next) => {
     })
     .catch(next)
 })
-
-// router.get('/calculations/:user_id/first', (req, res, next) => {
-//   Calculation.findOne({'user_id': req.params.user_id})
-//     .sort({_id: 1})
-//     .then((doc) => {
-//       return res.json(doc.calcData.result)
-//     })
-//     .catch(next)
-// })
-//
-// router.get('/calculations/last', (req, res, next) => {
-//   Calculation.findOne().sort({_id: -1}).lean().then((doc) => {
-//     return res.json(doc)
-//   }).catch(next)
-// })
-//
-// router.get('/calculations/all_info', (req, res, next) => {
-//   Calculation.find({})
-//     .select('_id user_id reqBody.location reqBody.time')
-//     .then(docsAll => {
-//       return filterAllInfo(docsAll).then((docsFiltered) => {
-//         return res.json(docsFiltered)
-//       })
-//     })
-//     .catch(next)
-// })
-//
-//
-// router.get('/calculations/:user_id/sessions', (req, res, next) => {
-//   Calculation.find({'user_id': req.params.user_id})
-//     .select('_id phone_id reqBody.location')
-//     .then((documents) => {
-//       return res.json(documents)
-//     })
-//     .catch(next)
-// })
 
 module.exports = router
